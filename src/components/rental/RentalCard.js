627239cc9209c51fb1873f3b133ebdd5a9c7ad3a
kushal atreya <kushalatreya@live.com>
2019-11-21 18:04:54 +0200

    Generates dynamic values to the card on all section, integrating the props from RentalList.js component

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -1,25 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function RentalCard() {
+export default function RentalCard(props) {
+  const rentalDetails = props.rentalDetails;
   return (
     <div className="col-md-3 col-xs-6">
-      <div className="card bwm-card">
-        <img
-          className="card-img-top"
-          src="http://via.placeholder.com/350x250"
-          alt=""
-        ></img>
-        <div className="card-block">
-          <h6 className="card-subtitle">Whole Apartment &#183; Kathmandu</h6>
-          <h4 className="card-title">Some nice apartment</h4>
-          <p className="card-text">
-            Rs: 24,000 per Month &#183; Free Cancelation
-          </p>
-          <a href="# " className="card-link">
-            More Info
-          </a>
+      <Link to={`/rentals/${rentalDetails.id}`}>
+        <div className="card bwm-card">
+          <img className="card-img-top" src={rentalDetails.image} alt=""></img>
+          <div className="card-block">
+            <h6 className="card-subtitle">
+              {rentalDetails.shared ? "shared" : "whole"}{" "}
+              {rentalDetails.category} &#183; {rentalDetails.city}
+            </h6>
+            <h4 className="card-title">{rentalDetails.title}</h4>
+            <p className="card-text">
+              Rs: {rentalDetails.dailyRate} per day &#183; Free Cancelation
+            </p>
+            {/* <a href="# " className="card-link">
+              More Info
+            </a> */}
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
