3519c892307046190985368499fbd7ca31a68889
kushal atreya <kushalatreya@live.com>
2019-12-04 12:26:13 +0200

    removes default styles from the link, adds styles to subtitles and title.

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -5,11 +5,15 @@ export default function RentalCard(props) {
   const rentalDetails = props.rentalDetails;
   return (
     <div className="col-md-3 col-xs-6">
-      <Link to={`/rentals/${rentalDetails.id}`}>
+      <Link className="rental-detail-link" to={`/rentals/${rentalDetails.id}`}>
         <div className="card bwm-card">
-          <img className="card-img-top" src={rentalDetails.image} alt=""></img>
+          <img
+            className="card-img-top"
+            src={rentalDetails.image}
+            alt="rental img"
+          ></img>
           <div className="card-block">
-            <h6 className="card-subtitle">
+            <h6 className={`card-subtitle ${rentalDetails.category}`}>
               {rentalDetails.shared ? "shared" : "whole"}{" "}
               {rentalDetails.category} &#183; {rentalDetails.city}
             </h6>
