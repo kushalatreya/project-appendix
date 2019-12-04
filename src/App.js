5147426a184feb421aa18c44b6f75498bd8f2100
kushal atreya <kushalatreya@live.com>
2019-12-04 11:40:15 +0200

    adds route and redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+import { Provider } from "react-redux";
+
 import Header from "./shared/Header";
 import RentalList from "./components/rental/RentalList";
+import RentalDetail from "./components/rental/RentalDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <RentalList />
-    </div>
-  );
+import { init } from "./reducers/index";
+
+import "./App.css";
+
+const store = init();
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+
+            <div className="container">
+              <Route
+                exact
+                path="/"
+                render={() => {
+                  return <Redirect to="/rentals" />;
+                }}
+              />
+              <Route exact path="/" component={RentalList} />
+              <Route exact path="/rentals" component={RentalList} />
+              <Route exact path="/rentals/:id" component={RentalDetail} />
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
